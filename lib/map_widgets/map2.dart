ffdcf0246ab5dbed72e8e0d0a49b2f9341f3bfd1
Almoatasem Belllah <almoatasem.bellah.haggag@gmail.com>
2020-03-07 00:57:36 +0200

    style1

--- lib/map_widgets/map2.dart
+++ lib/map_widgets/map2.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_gawlah/map_widgets/placecard.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -43,6 +44,7 @@ class PlacePolylineBody extends StatefulWidget {
 
 class PlacePolylineBodyState extends State<PlacePolylineBody>
     with TickerProviderStateMixin {
+      String mapstyle;
   PlacePolylineBodyState();
 
   String activepolygon = 'a';
@@ -65,182 +67,192 @@ class PlacePolylineBodyState extends State<PlacePolylineBody>
 
     polygons = new Set();
     polylines = new Set();
-
-    
-    super.initState();
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-    
-  }
-
-  void dispose() {
-    super.dispose();
-  }
-  
-
-  Future<void> gotoLocation(double lat, double long, double zoom) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-      target: LatLng(lat, long),
-      zoom: zoom,
-    )));
-  }
-
-  void _queryDatabase({String tag = 'all'}) {
-    if (tag == 'all') {
-      Query query = database.collection('polylines');
-      Mapobjects = query
-          .where("tours", arrayContains: widget.tour_id)
-          .snapshots()
-          .map((list) => list.documents.map((doc) => doc.data));
-    } else {
-      Query query =
-          database.collection('polylines').where('building', isEqualTo: tag);
-      Mapobjects = query
-          .snapshots()
-          .map((list) => list.documents.map((doc) => doc.data));
-    }
-
-    setState(() {
-      activeTag = tag;
-    });
-  }
-
-  LatLng _createcentre(GeoPoint centre) {
-    return new LatLng(centre.latitude, centre.longitude);
-  }
-
-  Set<Polygon> polygons_set(List polys, Set<Polygon> Polygons) {
-    Polygons.clear();
-
-    polys.forEach((PolyObj) {
-      switch (PolyObj['type']) {
-        case 'place':
-          Polygons.add(Place(PolyObj['points'], PolyObj['name']));
-          break;
-
-        default:
-          break;
+    rootBundle.loadString('images_and_icons/mapstyle.txt').then((string){
+      mapstyle = string;
+          
+        });
+       controller.setMapStyle(mapstyle);        
+        
+        
+        super.initState();
       }
-    });
-
-    return Polygons;
-  }
-
-  Set<Polyline> polylines_set(List polyys, Set<Polyline> Polylines) {
-    Polylines.clear();
-
-    polyys.forEach((PolyObj) {
-      switch (PolyObj['type']) {
-        case 'route':
-          Polylines.add(Route(PolyObj['points'], PolyObj['name']));
-          break;
-
-        default:
-          break;
+    
+    
+    
+      void _onMapCreated(GoogleMapController controller) {
+        _controller.complete(controller);
+        
+        
       }
-    });
-
-    return Polylines;
-  }
-
-  Polygon Place(List<dynamic> polylinePoints, String idd) {
-    List<LatLng> latlngs = new List();
-    polylinePoints.forEach((point) {
-      latlngs.add(new LatLng(
-          (point as GeoPoint).latitude, (point as GeoPoint).longitude));
-    });
-
-    return new Polygon(
-      consumeTapEvents: false,
-      polygonId: PolygonId(idd),
-      fillColor: activepolygon == idd ? Colors.red : Colors.transparent,
-      strokeColor: Colors.black,
-      strokeWidth: 5,
-      visible: true,
-      points: latlngs,
-    );
-  }
-
-  Polyline Route(List<dynamic> polylinePoints, String idd) {
-    List<LatLng> latlngs = new List();
-    polylinePoints.forEach((point) {
-      latlngs.add(new LatLng(
-          (point as GeoPoint).latitude, (point as GeoPoint).longitude));
-    });
-
-    return new Polyline(
-      consumeTapEvents: false,
-      polylineId: PolylineId(idd),
-      color: Colors.greenAccent,
-      width: 4,
-      visible: true,
-      points: latlngs,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<Object>(
-          stream: Mapobjects,
-          builder: (context, AsyncSnapshot snapshot) {
-            List slideList = snapshot.data.toList();
-            return Stack(
-              children: <Widget>[
-                Center(
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: GoogleMap(
-                      
-                      mapType: MapType.normal,
-                      
-                      initialCameraPosition: new CameraPosition(
-                      target: _createcentre(widget.centre), zoom: 15.5),
-                      polygons: polygons_set(slideList, polygons),
-                      polylines: polylines_set(slideList, polylines),
-                      onMapCreated: _onMapCreated,
+    
+      void dispose() {
+        super.dispose();
+      }
+      
+    
+      Future<void> gotoLocation(double lat, double long, double zoom) async {
+        final GoogleMapController controller = await _controller.future;
+        controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target: LatLng(lat, long),
+          zoom: zoom,
+        )));
+      }
+    
+      void _queryDatabase({String tag = 'all'}) {
+        if (tag == 'all') {
+          Query query = database.collection('polylines');
+          Mapobjects = query
+              .where("tours", arrayContains: widget.tour_id)
+              .snapshots()
+              .map((list) => list.documents.map((doc) => doc.data));
+        } else {
+          Query query =
+              database.collection('polylines').where('building', isEqualTo: tag);
+          Mapobjects = query
+              .snapshots()
+              .map((list) => list.documents.map((doc) => doc.data));
+        }
+    
+        setState(() {
+          activeTag = tag;
+        });
+      }
+    
+      LatLng _createcentre(GeoPoint centre) {
+        return new LatLng(centre.latitude, centre.longitude);
+      }
+    
+      Set<Polygon> polygons_set(List polys, Set<Polygon> Polygons) {
+        Polygons.clear();
+    
+        polys.forEach((PolyObj) {
+          switch (PolyObj['type']) {
+            case 'place':
+              Polygons.add(Place(PolyObj['points'], PolyObj['name']));
+              break;
+    
+            default:
+              break;
+          }
+        });
+    
+        return Polygons;
+      }
+    
+      Set<Polyline> polylines_set(List polyys, Set<Polyline> Polylines) {
+        Polylines.clear();
+    
+        polyys.forEach((PolyObj) {
+          switch (PolyObj['type']) {
+            case 'route':
+              Polylines.add(Route(PolyObj['points'], PolyObj['name']));
+              break;
+    
+            default:
+              break;
+          }
+        });
+    
+        return Polylines;
+      }
+    
+      Polygon Place(List<dynamic> polylinePoints, String idd) {
+        List<LatLng> latlngs = new List();
+        polylinePoints.forEach((point) {
+          latlngs.add(new LatLng(
+              (point as GeoPoint).latitude, (point as GeoPoint).longitude));
+        });
+    
+        return new Polygon(
+          consumeTapEvents: false,
+          polygonId: PolygonId(idd),
+          fillColor: activepolygon == idd ? Colors.red : Colors.transparent,
+          strokeColor: Colors.black,
+          strokeWidth: 5,
+          visible: true,
+          points: latlngs,
+        );
+      }
+    
+      Polyline Route(List<dynamic> polylinePoints, String idd) {
+        List<LatLng> latlngs = new List();
+        polylinePoints.forEach((point) {
+          latlngs.add(new LatLng(
+              (point as GeoPoint).latitude, (point as GeoPoint).longitude));
+        });
+    
+        return new Polyline(
+          consumeTapEvents: false,
+          polylineId: PolylineId(idd),
+          color: Colors.greenAccent,
+          width: 4,
+          visible: true,
+          points: latlngs,
+        );
+      }
+    
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          body: StreamBuilder<Object>(
+              stream: Mapobjects,
+              builder: (context, AsyncSnapshot snapshot) {
+                List slideList = snapshot.data.toList();
+                return Stack(
+                  children: <Widget>[
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        child: GoogleMap(
+                          
+                          mapType: MapType.normal,
+                          
+                          initialCameraPosition: new CameraPosition(
+                          target: _createcentre(widget.centre), zoom: 15.5),
+                          polygons: polygons_set(slideList, polygons),
+                          polylines: polylines_set(slideList, polylines),
+                          onMapCreated: _onMapCreated,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Positioned(
-                    height: 250,
-                    
-                    width: MediaQuery.of(context).size.width,
-                    bottom: 0,
-                    child: PageView.builder(
-                        physics: BouncingScrollPhysics(),
-                        controller: PageController(
-                            viewportFraction: 0.59, initialPage:0),
-                        onPageChanged: (int index) {
-                          setState(() {
-                            activepolygon = slideList[index]['name'];
-                            gotoLocation(
-                                ((slideList[index]['center']) as GeoPoint)
-                                    .latitude,
-                                ((slideList[index]['center']) as GeoPoint)
-                                    .longitude,
-                                18.0);
-                          });
-                        },
-                        itemCount: slideList.length,
-                        itemBuilder: (context, index) {
-                          {
-                            if (slideList[index]['type'] == 'place') {
-                              return PlaceCard(
-                                image: slideList[index]['image'],
-                                name: slideList[index]['name'],
-                                placetype: slideList[index]['placetype'],
-                              );
-                            }
-                          }
-                        })),
-              ],
-            );
-          }),
-    );
-  }
-}
+                    Positioned(
+                        height: 250,
+                        
+                        width: MediaQuery.of(context).size.width,
+                        bottom: 0,
+                        child: PageView.builder(
+                            physics: BouncingScrollPhysics(),
+                            controller: PageController(
+                                viewportFraction: 0.59, initialPage:0),
+                            onPageChanged: (int index) {
+                              setState(() {
+                                activepolygon = slideList[index]['name'];
+                                gotoLocation(
+                                    ((slideList[index]['center']) as GeoPoint)
+                                        .latitude,
+                                    ((slideList[index]['center']) as GeoPoint)
+                                        .longitude,
+                                    18.0);
+                              });
+                            },
+                            itemCount: slideList.length,
+                            itemBuilder: (context, index) {
+                              {
+                                if (slideList[index]['type'] == 'place') {
+                                  return PlaceCard(
+                                    image: slideList[index]['image'],
+                                    name: slideList[index]['name'],
+                                    placetype: slideList[index]['placetype'],
+                                  );
+                                }
+                              }
+                            })),
+                  ],
+                );
+              }),
+        );
+      }
+    }
+    
+
